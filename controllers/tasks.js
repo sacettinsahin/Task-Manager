d8f95fa2ec921520e57df38b2c30674a897c3fe0
Sacettin Şahin <sacettin.sahin@etiya.com>
2023-05-29 18:02:23 +0300

    updateTask method was completed

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -49,13 +49,26 @@ const getTask = async (req, res) => {
 //   }
 // };
 
-const updateTask = (req, res) => {
-  res.send("update task");
-};
 const deleteTask = async (req, res) => {
   try {
     const { id: taskID } = req.params;
     const task = await Task.findOneAndDelete({ _id: taskID });
+    if (!task) {
+      res.status(404).json({ message: `No task with id: ${taskID}` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
+};
+
+const updateTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+      new: true,
+      runValidators: true,
+    });
     if (!task) {
       res.status(404).json({ message: `No task with id: ${taskID}` });
     }
