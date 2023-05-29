40878a4ce5d8733882e953026019e3224003a0b2
Sacettin Şahin <sacettin.sahin@etiya.com>
2023-05-29 14:05:39 +0300

    other of crud methods created
    Main structure was created and added other methods

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -2,6 +2,24 @@ const getAllTasks = (req, res) => {
   res.send("Get All Items from contoller");
 };
 
+const createTask = (req, res) => {
+  res.send("create task");
+};
+
+const getTask = (req, res) => {
+  res.send("get single task");
+};
+const updateTask = (req, res) => {
+  res.send("update task");
+};
+const deleteTask = (req, res) => {
+  res.send("delete task");
+};
+
 module.exports = {
   getAllTasks,
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask,
 };
