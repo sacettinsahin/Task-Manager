7483150b3c0fb833758ac5599e029fe35d949d39
Sacettin Şahin <sacettin.sahin@etiya.com>
2023-05-29 16:02:43 +0300

    basic validations were created

--- models/Task.js
+++ models/Task.js
@@ -1,8 +1,16 @@
 const mongoose = require("mongoose");
 
 const TaskSchema = new mongoose.Schema({
-  name: String,
-  completed: Boolean,
+  name: {
+    type: String,
+    required: [true, "must provide name"], //validasyon mesajı göndermek için
+    trim: true, //stringin başındaki ve sonundaki boşlukları siler "    sahin    "--> "sahin"
+    maxlength: [20, "name can not be more than 20 characters"],
+  },
+  completed: {
+    type: Boolean,
+    default: false, //eğer girilmezse false gönder
+  },
 });
 
 module.exports = mongoose.model("Task", TaskSchema);
