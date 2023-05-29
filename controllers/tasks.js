c043a0daaf1f42c5530c764fa104a23eadb7a275
Sacettin Şahin <sacettin.sahin@etiya.com>
2023-05-29 13:51:03 +0300

    Route and Controller Structure
    The structure was created for getAllTasks

---
+++ controllers/tasks.js
@@ -0,0 +1,7 @@
+const getAllTasks = (req, res) => {
+  res.send("Get All Items from contoller");
+};
+
+module.exports = {
+  getAllTasks,
+};
--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const app = express();
+const tasks = require("./routes/tasks");
+
+// middleware
+app.use(express.json());
 
 //routes
-app.get("/hello", (req, res) => {
+app.get("/", (req, res) => {
   res.send("task manager app");
 });
+
+app.use("/api/v1/tasks", tasks);
+
+// app.get("/api/v1/tasks")
+// app.post("/api/v1/tasks")
+// app.get("/api/v1/tasks/:id")
+// app.patch("/api/v1/tasks/:id")
+// app.delete("/api/v1/tasks/:id")
 
 const port = 3000;
 
