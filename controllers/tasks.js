78153647ab54561d38712dce10dec3ac76e1d2e9
Sacettin Şahin <sacettin.sahin@etiya.com>
2023-05-29 17:36:34 +0300

    deleteTask method was completed.

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -18,6 +18,8 @@ const createTask = async (req, res) => {
   }
 };
 
+//!idnin lengthinden farklı bi değer post edilirse catch bloğu çalışıyor, aynı syttaxta olmayan değer gönderilirse
+//o zaman da if(!task) bloğu çalışıyor
 const getTask = async (req, res) => {
   try {
     const { id: taskID } = req.params; //const taskID = req.params.id;
@@ -50,8 +52,17 @@ const getTask = async (req, res) => {
 const updateTask = (req, res) => {
   res.send("update task");
 };
-const deleteTask = (req, res) => {
-  res.send("delete task");
+const deleteTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndDelete({ _id: taskID });
+    if (!task) {
+      res.status(404).json({ message: `No task with id: ${taskID}` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
 };
 
 module.exports = {
