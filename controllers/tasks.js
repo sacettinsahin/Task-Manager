16709d4041ca640708419a43b557feb656214a39
Sacettin Şahin <sacettin.sahin@etiya.com>
2023-05-29 15:03:57 +0300

    Connected to MongoDB and created .env file
    .env and /node_modules were writed to gitignore file and connected to DB

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -3,11 +3,11 @@ const getAllTasks = (req, res) => {
 };
 
 const createTask = (req, res) => {
-  res.send("create task");
+  res.json(req.body);
 };
 
 const getTask = (req, res) => {
-  res.send("get single task");
+  res.json({ id: req.params.id });
 };
 const updateTask = (req, res) => {
   res.send("update task");
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
+require("dotenv").config();
 
 // middleware
 app.use(express.json());
@@ -18,6 +20,16 @@ app.use("/api/v1/tasks", tasks);
 // app.patch("/api/v1/tasks/:id")
 // app.delete("/api/v1/tasks/:id")
 
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
 
 app.listen(port, console.log(`server is listening on ${port}...`));
